750f7ac00e1b9486df5de8f48c44e09b5dc88cc9
Ekim Emre Yardimli <ekimyardimli@izlenim.com>
2024-05-03 00:27:49 +0800

    Update wind.py

--- wind.py
+++ wind.py
@@ -1,11 +1,33 @@
 from gpiozero import Button
+import time
+import math
 
-wind_speed_sensor = Button(5)
 wind_count = 0
+radius_cm = 9.0
+wind_interval = 5
 
 def spin():
     global wind_count
     wind_count = wind_count + 1
-    print("spin" + str(wind_count))
 
+def calculate_speed(time_sec):
+    global wind_count
+    circumference_cm = (2 * math.pi) * radius_cm
+    rotations = wind_count / 2.0
+
+    dist_cm = circumference_cm * rotations
+
+    speed = dist_cm / wind_interval
+
+    return speed
+
+
+wind_speed_sensor = Button(5)
 wind_speed_sensor.when_pressed = spin
+spin()
+spin()
+
+while true:
+    wind_count = 0
+    time.sleep(wind_interval)
+    print( calculate_speed(wind_interval), "cm/h")
